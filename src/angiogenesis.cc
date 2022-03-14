676e461abd9372a516ada3c8dc12547cfab863f5
Tobias Duswald <tobias.duswald@cern.ch>
2022-03-14 17:48:55 +0100

    Update Copyright

--- src/angiogenesis.cc
+++ src/angiogenesis.cc
@@ -1,7 +1,6 @@
 // -----------------------------------------------------------------------------
 //
-// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
-// BioDynaMo collaboration. All Rights Reserved.
+// Copyright (C) 2022 CERN, TUM, and UT Austin. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
