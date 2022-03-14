2da9ddbcc385143cc9f92073d0500b25ac7008db
Tobias Duswald <tobias.duswald@cern.ch>
2022-03-14 17:33:30 +0100

    "Initial commit"

---
+++ src/angiogenesis.h
@@ -0,0 +1,39 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+#ifndef ANGIOGENESIS_H_
+#define ANGIOGENESIS_H_
+
+#include "biodynamo.h"
+
+namespace bdm {
+
+inline int Simulate(int argc, const char** argv) {
+  Simulation simulation(argc, argv);
+
+  // Define initial model - in this example: single cell at origin with a
+  // diameter of 30.0 (no unit associated).
+  auto* rm = simulation.GetResourceManager();
+  auto* cell = new Cell(30.0);
+  rm->AddAgent(cell);
+
+  // Run simulation for one timestep
+  simulation.GetScheduler()->Simulate(1);
+
+  std::cout << "Simulation completed successfully!" << std::endl;
+  return 0;
+}
+
+}  // namespace bdm
+
+#endif  // ANGIOGENESIS_H_
