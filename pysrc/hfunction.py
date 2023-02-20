e9f50cb1cd382a26626ccaf3417792ed66d8ac0c
Tobias Duswald <tobias.duswald@cern.ch>
2023-02-20 13:26:33 +0100

    Add approximation to h plot

--- pysrc/hfunction.py
+++ pysrc/hfunction.py
@@ -14,10 +14,16 @@ flags.DEFINE_float("a", 0.1, "a")
 flags.DEFINE_float("b", 0.1, "b")
 flags.DEFINE_float("gamma", 1, "gamma")
 flags.DEFINE_float("dt", 0.1, "dt")
+flags.DEFINE_boolean("approx", False, "Plot approximation?")
 
 # Helper function h, smooth heaviside function
 def h(x, a, b, gamma, xx, dt):
     return 1 - np.exp(-(a + 1 / (gamma + np.exp(2 * b * (x - xx)))) * dt)
+
+
+# Approximation of h
+def h_approx(x, a, b, gamma, xx, dt):
+    return (a + 1 / (gamma + np.exp(2 * b * (x - xx)))) * dt
 
 
 def main(argv):
@@ -27,6 +33,8 @@ def main(argv):
     aa = FLAGS.a
     bb = FLAGS.b
     gamma = FLAGS.gamma
+    plot_approximation = FLAGS.approx
+
     print("xbar = {}".format(xxx))
     print("a = {}".format(aa))
     print("b = {}".format(bb))
@@ -37,7 +45,12 @@ def main(argv):
     x = np.linspace(0, 1, 500)
     sns.set()
     plt.rc("text", usetex=True)
-    plt.plot(x, h(x, aa, bb, gamma, xxx, dt))
+    plt.plot(x, h(x, aa, bb, gamma, xxx, dt), label=r"$h(x)$")
+    if plot_approximation:
+        plt.plot(
+            x, h_approx(x, aa, bb, gamma, xxx, dt), label=r"$h_{approx}(x)$"
+        )
+        plt.legend()
     plt.xlabel(r"$x$")
     plt.ylabel(r"$h(x;a,b,\bar{x})$")
     plt.show()
