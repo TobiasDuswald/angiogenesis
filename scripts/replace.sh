9ddd733dfefbe49e1dcf1da54f7bc5d3c5788173
Tobias Duswald <tobias.duswald@cern.ch>
2023-02-21 09:49:48 +0100

    Make replace command safer

--- scripts/replace.sh
+++ scripts/replace.sh
@@ -7,9 +7,26 @@
 # 2. The string to be replaced
 # 3. The string to replace the first string
 function replace() {
+    # Verify that the function is called with the correct number of arguments
+    if [[ $# -ne 3 ]]; then
+        echo "Usage: replace <file> <string to replace> <replacement string>"
+        return 1
+    fi
+    # Check if <string to replace> is in <file>
+    if ! grep -q "$2" "$1"; then
+        echo "String '$2' not found in file '$1'"
+        return 1
+    fi
+    # Call sed with the appropriate arguments
     if [[ "$OSTYPE" == "darwin"* ]]; then
         sed -i '' "s/$2/$3/g" $1
     else
         sed -i "s/$2/$3/g" $1
     fi
+    # Check if the replacement was successful
+    if ! grep -q "$3" "$1"; then
+        echo "Replacement failed"
+        return 1
+    fi
+    return 0
 }
